75bfdfa6904aa8fc852a4ea8f0fdee40170cef92
Abhijeet Sridhar M <abhhijeetsridhar14@gmail.com>
2018-11-10 12:11:47 +0530

    Recognized and removed the utf-8 encoding. Recognizing copyright symbol etc. without any trouble

--- pdfToText/recognize text from image.py
+++ pdfToText/recognize text from image.py
@@ -1,9 +1,10 @@
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
-text = str(((pytesseract.image_to_string(Image.open('img_1.jpg'))).encode('utf-8')))
-text = text.replace('-\\n', '')
-text = text.replace('\\n', ' ')
+text = str(((pytesseract.image_to_string(Image.open('img_1.jpg')))))
+#text = str(((pytesseract.image_to_string(Image.open('img_1.jpg'))).encode('utf-8')))
+text = text.replace('-\n', '')
+text = text.replace('\n', ' ')
 
 
 f=open("abstract.txt", "r+")
