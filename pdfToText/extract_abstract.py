57527b79fbc87c951ec29851fce8edd24043d3c8
ABHIJEET-SRIDHAR-M <abhijeetsridhar14@gmail.com>
2018-12-10 22:51:10 +0530

    python requirements changed, changed python to python3

--- pdfToText/extract_abstract.py
+++ pdfToText/extract_abstract.py
@@ -2,18 +2,18 @@ import os.path
 import sys
 pdf = 'd.pdf'
 
-os.system('python "storing pic of first page of pdf.py" '+pdf)
+os.system('python3 "storing pic of first page of pdf.py" '+pdf)
 
 if os.path.exists('out.jpg') == False :
 	print("Error in taking pic of the first page of the pdf")
 	sys.exit()
 
 
-os.system('python "drawing boxes and storing.py"')
+os.system('python3 "drawing boxes and storing.py"')
 
 if os.path.exists('img_1.jpg') == False :
 	print("Error in Drawing bounding box")
 	sys.exit()
 
 
-os.system('python "recognize text from image.py"')	
+os.system('python3 "recognize text from image.py"')	
